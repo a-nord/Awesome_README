1b55c2e4c09413661364b865cc875eef9f4c54b0
Ashley Nord <anord@hntb.com>
2024-01-19 19:33:52 -0500

    added some questions and updated the generate markdown file

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the TITLE of the project?",
+    name: "title",
+  },
+
+  {
+    type: "input",
+    message: "What is the DESCRIPTION of the project? (Include the motivation, reasoning, what it solves, and what you learned.')",
+    name: "description",
+  },
+
+  {
+    type: "input",
+    message: "What is your github USERNAME?",
+    name: "username",
+  },
+
+  {
+    type: "input",
+    message: "What is the github REPO NAME?",
+    name: "repoName",
+  },
+
+  {
+    type: "input",
+    message: "What is your LINKEDIN (username)?",
+    name: "linkedin",
+  },
+
+  {
+    type: "list",
+    message: "What kind of LICENSE do you want?",
+    name: "license",
+    choices: ["APACHE 2.0","Mozilla","MIT","BSD 2", "BSD 3","Boost", "none"],
+  }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    const contents = generateMarkdown(data);
+    fs.writeToFile(fileName, contents,(err)=>{
+        err? new Error("Write file does not like you!!"): console.log("It's you, not the write file!");
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+   const answers = await inquirer.promt(questions);
+   writeToFile('README.md', answers)
+}
 
 // Function call to initialize app
 init();
+ 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,17 +4,58 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `[https://choosealicense.com/](https://choosealicense.com/)`
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `##License
+  ${renderLicenseLink(license)}
+  ${renderLicenseBadge(license)}`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `#${data.title}
 
-`;
+  ## Description
+  ${data.description}
+    
+  ## Table of Contents (Optional)
+  
+  If your README is long, add a table of contents to make it easy for users to find what they need.
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation  
+  ${data.install}
+  
+  ## Usage  
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+    
+  ${renderLicenseSection(${data.license})}
+  
+  ## Badges
+  
+  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+  
+  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  
+  ## How to Contribute
+  
+  ${data.contribute}
+  
+  ## Tests
+  
+  ${data.test}`;
 }
 
 module.exports = generateMarkdown;
