1620bb8849e3ea662d648e7fe01118b9644182b0
Ashley Nord <anord@hntb.com>
2024-01-24 23:17:45 -0500

    added comments to generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch (license) {
@@ -23,7 +23,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   switch (license) {
@@ -50,14 +50,14 @@ function renderLicenseLink(license) {
 };
 
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   return `## License
   ${renderLicenseBadge(license)}${renderLicenseLink(license)}`
 }
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
